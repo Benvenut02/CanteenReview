57e5094998e0b0afc08d60d90de206f8ff7e23e7
Ashish <ashish.soren@xaviers.edu.in>
2026-08-31 19:51:59 +0530

    Initial Commit

---
+++ Data/CanteenContext.cs
@@ -0,0 +1,13 @@
+﻿using CanteenReview.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanteenReview.Data
+{
+    public class CanteenContext : DbContext
+    {
+        public CanteenContext(DbContextOptions<CanteenContext> options) : base(options) { }
+
+        public DbSet<FoodItem> FootItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ Models/CanteenModels.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanteenReview.Models
+{
+    public class FoodItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        // This links the FoodItem to its reviews
+        public List<Review> Reviews { get; set; }
+    }
+
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string StudentName { get; set; } // Keep it simple, just ask for a name
+
+        [Range (1, 5, ErrorMessage = "Rating must be between 1 and 5 stars.")]
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        // Foreign key linking back to the FoodItem
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; }
+    }
+}
